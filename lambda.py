a90d8dbe8a70efcb087a789e0d2f67cb39ac7e54
gugugagagagagaga <146242047+gugugagagagagaga@users.noreply.github.com>
2023-11-16 21:49:59 +0100

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,18 @@
+#def zad1(lst1, lst2, les3):
+#       sr = lambda lst: sum(lst)/len(lst)
+#    all_lst = [lst1,lst2,les3]
+#w = []
+#for lst in all_lst:
+#        w.append(sr(lst))
+#return w.index(max(w))
+#print(zad1([1,2,3,4,5],[-2,-3,-6],[20]))
+
+#from random import randint
+#mabababaa = lambda k,n,m:[randint(n,m) for i in range(k)]
+#print(mabababaa(10,10,20))
+
+#asffas = lambda lst: list(map(lambda el: el**2, lst))
+#print(asffas([2,3,4,5,6,7,8]))
+ 
+aasfa = lambda lst: list(map(lambda el: el**1/2, lst))
+print(aasfa([3,2,10]))
